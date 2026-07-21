74b694366661d6227d821974fa918bcfeedbdd72
AnumFatima-8389 <anfatima.bscs24seecs@seecs.edu.pk>
2026-07-21 11:38:54 +0500

    Stage 2: insert into database

--- A2_Connecting_to_database/main.py
+++ A2_Connecting_to_database/main.py
@@ -1,8 +1,15 @@
 
 import sqlite3   
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse 
-app = FastAPI()
+from fastapi.responses import JSONResponse  
+from pydantic import BaseModel  
+class Task(BaseModel): 
+    title : str
+    done : bool
+    
+app = FastAPI() 
+
+# function for getting all data
 def returnData(tablename): 
     connection = sqlite3.connect("tasks.db") 
     cursor = connection.cursor() 
@@ -15,7 +22,8 @@ def returnData(tablename):
             "title": d[1],
             "done": bool(d[2])
         }) 
-    return datalist 
+    return datalist  
+# function for getting data of a particular id 
 def returnDetailsOfId(tablename,id):
     connection = sqlite3.connect("tasks.db") 
     cursor = connection.cursor() 
@@ -38,11 +46,11 @@ def returnDetailsOfId(tablename,id):
     
 connection = sqlite3.connect("tasks.db") 
 cursor = connection.cursor() 
-
+# creating table if it doesn't already exist
 cursor.execute(
     """
         CREATE TABLE IF NOT EXISTS tasks (
-            id integer primary key, 
+            id integer primary key autoincrement , 
             title text, 
             done boolean 
         )
@@ -53,7 +61,8 @@ print("Table created")
 cursor.execute(
     "select exists(select 1 from tasks)"
 ) 
-hasdata = cursor.fetchone()[0] 
+hasdata = cursor.fetchone()[0]  
+# adding data if it doesn't have any already 
 if not hasdata: 
     cursor.execute("""
             INSERT INTO tasks 
@@ -66,15 +75,36 @@ if not hasdata:
     print("data added")
 else: 
     print("Data already exists so no starter-data added")  
-
+# get all tasks
 @app.get("/tasks") 
 def getTasks():
     return{
         "title":"All tasks", 
         "individual tasks":returnData("tasks")
-    } 
+    }  
+    
+# get tasks based on id 
 @app.get("/tasks/{id}") 
 def getTaskById(id:int): 
-    return returnDetailsOfId("tasks",id)
+    return returnDetailsOfId("tasks",id)  
+
+# post a new task 
+@app.post("/task") 
+def addTask(task : Task): 
+    connection = sqlite3.connect('tasks.db') 
+    cursor = connection.cursor()
+    cursor.execute(
+        f"""
+            insert into tasks (title,done)
+            values (?,?)
+        """,(task.title,task.done)     
+    )  
+    connection.commit()
+    return {
+        "message":"Task added successfully"
+    }
+    
+    
+
     
 
