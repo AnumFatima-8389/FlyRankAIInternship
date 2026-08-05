3e2d2047a1edfe57bc19127a4fe8ef5a1d2a036c
AnumFatima-8389 <anfatima.bscs24seecs@seecs.edu.pk>
2026-08-05 05:07:13 +0500

    Stage 1: signup and login routes working

--- Assignment4_supabaseAuthentication/main.py
+++ Assignment4_supabaseAuthentication/main.py
@@ -2,7 +2,14 @@ from contextlib import asynccontextmanager
 import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from supabase import create_client
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from supabase import AuthError, create_client 
+from pydantic import BaseModel 
+
+class Credentials(BaseModel): 
+    email:str 
+    password : str 
 
 load_dotenv()
 
@@ -16,10 +23,44 @@ supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 async def lifespan(app: FastAPI):
    print("Server running and connected to supabase") 
    yield 
-   print("After yield")
    
 app = FastAPI(lifespan=lifespan)
 
-@app.get("/")
-def home():
-    return {"message": "Server is running"} 
+@app.post("/auth/signup")
+def signup(credentials:Credentials): 
+    if not credentials.email or not credentials.password: 
+        return JSONResponse(
+            status_code = 400, 
+            content = {"message":"Missing email or password."}
+        )
+    res = supabase.auth.sign_up({'email':credentials.email, 'password' : credentials.password}) 
+    return JSONResponse(
+        status_code=201, 
+        content=jsonable_encoder({"user":res.user.model_dump()})
+    )  
+@app.post("/auth/login") 
+def login(credentials:Credentials): 
+    if not credentials.email or not credentials.password: 
+        return JSONResponse(
+            status_code = 400, 
+            content = {"message":"Missing email or password."}
+        )  
+    try:
+        res = supabase.auth.sign_in_with_password({
+            'email':credentials.email, 
+            'password':credentials.password
+        })  
+        session = res.session
+        return JSONResponse(
+            status_code = 200, 
+            content = {
+                'access token': session.access_token, 
+                'refresh token':session.refresh_token
+            }
+        ) 
+    except AuthError as e: 
+        return JSONResponse(
+            status_code = 401, 
+            content = { "error": "Invalid login credentials" }
+        )
+        
